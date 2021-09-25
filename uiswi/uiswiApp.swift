26379c0701ed790f0fb0b3efef3d11efb90ae08c
Taeh83 <dreamsky83@naver.com>
2021-09-25 16:05:44 +0900

    Initial Commit

---
+++ uiswi/uiswiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiswiApp.swift
+//  uiswi
+//
+//  Created by 홍태희 on 2021/09/25.
+//
+
+import SwiftUI
+
+@main
+struct uiswiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
